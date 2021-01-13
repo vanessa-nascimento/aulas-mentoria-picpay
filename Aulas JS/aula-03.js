88d84f2dc59336eb9f6576c217982b5234c293a2
vanascimento <vanessa.nascimento@picpay.com>
2021-01-13 09:58:42 -0300

    Aula 3 de JS

---
+++ Aulas JS/aula-03.js
@@ -0,0 +1,22 @@
+function Blab() { return "Palhaça" }
+
+document.querySelector("h1").textContent = Blab()
+
+
+var Vanessa = "Blab"
+
+Vanessa = "Cachorro";
+
+
+// arrow function x function
+()=> "Bla" //arrow function: return está implícito aqui, não precisa declarar
+
+//funcao anonima
+function() {
+    return "Bla"
+}
+
+//fucao nomeada
+function Ola() {
+    return "Olá!"
+}
